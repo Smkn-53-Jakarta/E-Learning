ed12fadbdf59e15d7fd07ed60bb7c9133856b00f
CROWNIX <rahmatfauzi841@gmail.com>
2024-05-23 20:15:48 +0700

    chore: adjustment view list courses

--- resources/views/admin/courses/index.blade.php
+++ resources/views/admin/courses/index.blade.php
@@ -68,8 +68,8 @@
                                             <tr>
                                                 <td class="text-center">{{ $loop->iteration }}</td>
                                                 <td>{{ $course->name }}</td>
-                                                <td>-</td>
-                                                <td>-</td>
+                                                <td>{{ $teacher->created_at->diffForHumans() }}</td>
+                                                <td>{{ $teacher->updated_at->diffForHumans() }}</td>
                                                 <td>
                                                     <a href="#"
                                                         class="btn btn-sm btn-icon btn-active-light-primary"
