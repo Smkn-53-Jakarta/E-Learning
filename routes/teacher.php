05e29129dca2b8ff70fa11673f9f4ec9dd6c1943
RIDWANKING11 <muhammadridwanking@gmail.com>
2024-06-03 15:07:13 +0700

    - membuat controller resource materi - menambahkan  permission seed edit - menambahkan url views index menuju edit - membuat routes resource materi

--- routes/teacher.php
+++ routes/teacher.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Teacher\AttendanceController;
+use App\Http\Controllers\Teacher\MaterialController;
 use App\Http\Controllers\Teacher\TeachingScheduleController;
 use Illuminate\Support\Facades\Route;
 
@@ -8,16 +9,21 @@ Route::prefix('guru')->middleware('auth', 'permissions')->group(function () {
     Route::get('/dashboard', function () {
         return view('teachers.dashboard');
     })->name('teacher-dashboard.index');
-
+    
     Route::get('/jadwal-mengajar', [TeachingScheduleController::class, 'index'])->name('teacher-teaching-schedules.index');
 
     Route::get('/jadwal-mengajar/{scheduleOfSubject}/kehadiran', [AttendanceController::class, 'index'])->name('teacher-attendances.index');
+    
 
-    Route::get('jadwal-mengajar/ruang-materi', function () {
-        return view('teachers.materials.index');
-    })->name('teacher-materials.index');
+    Route::get('jadwal-mengajar/ruang-materi/trashed', [MaterialController::class, 'trashed'])
+        ->name('teacher-materials.trashed');
+    Route::resource('jadwal-mengajar/ruang-materi', MaterialController::class)
+        ->only(['index', 'create', 'edit'])
+        ->parameters(['ruang-materi' => 'material'])
+        ->names([
+            'index' => 'teacher-materials.index',
+            'create' => 'teacher-materials.create',
+            'edit' => 'teacher-materials.edit',
+        ]);
 
-    Route::get('jadwal-mengajar/ruang-materi/tambah-materi', function(){
-        return view('teachers.materials.create');
-    })->name('teacher-materials.create');
 });
