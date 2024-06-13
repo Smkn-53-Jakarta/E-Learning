16448d35a976bb63ffff12d5c145cc82c7f8e041
RIDWANKING11 <muhammadridwanking@gmail.com>
2024-06-13 20:23:47 +0700

    docs: menambahkan logo pada url

--- resources/views/teachers/e-raports/generate.blade.php
+++ resources/views/teachers/e-raports/generate.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>E-Raport</title>
+    <link rel="shortcut icon" href="{{ FileHelper::getImage('images/logo/logo.png') }}" />
     <link href="{{ asset('assets/template') }}/plugins/global/plugins.bundle.css" rel="stylesheet" type="text/css" />
     <link href="{{ asset('assets/template') }}/css/style.bundle.css" rel="stylesheet" type="text/css" />
     <link rel="stylesheet" href="{{ asset('assets/css/app.css') }}">
