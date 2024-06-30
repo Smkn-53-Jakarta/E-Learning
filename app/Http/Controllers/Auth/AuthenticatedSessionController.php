036932d57894d0b43ec2a54e60a0c03d5b92ec2d
CROWNIX <rahmatfauzi841@gmail.com>
2024-06-30 23:16:30 +0700

    chore: change redirect url when logout

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -78,6 +78,6 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerateToken();
 
-        return redirect('/');
+        return redirect('/login');
     }
 }
